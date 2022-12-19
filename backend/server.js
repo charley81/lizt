3f8631a660af5e5654732b84192a3473884ae637
ChrisKnowsDev <chrisharley81@gmail.com>
2022-12-19 11:15:51 -0500

    mongoDB connected to server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { connectDB } from './config/db.js'
 
 const app = express()
 dotenv.config()
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
+connectDB()
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`))
