3c4c2f5306d79446171a1ada546f46953663833e
ChrisKnowsDev <chrisharley81@gmail.com>
2023-01-09 16:45:00 -0500

    initial styling for list complete

--- frontend/src/components/list-section/list-section.tsx
+++ frontend/src/components/list-section/list-section.tsx
@@ -10,6 +10,22 @@ export class ListSection {
   @Prop() tasks: Task[] = [];
 
   render() {
-    return <ul>{!this.tasks.length ? <li>no task to show</li> : this.tasks.map(item => <li>{item.task}</li>)}</ul>;
+    return (
+      <ul>
+        {!this.tasks.length ? (
+          <li>no task to show</li>
+        ) : (
+          this.tasks.map(item => (
+            <li>
+              {item.task}
+              <span class="icons">
+                <ion-icon name="create-outline" class="edit"></ion-icon>
+                <ion-icon name="trash-outline" class="delete"></ion-icon>
+              </span>
+            </li>
+          ))
+        )}
+      </ul>
+    );
   }
 }
